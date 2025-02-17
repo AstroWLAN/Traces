2f1003d08766986f1cf590ff664145e4832c7d4d
AstroWLAN <dario2.crippa@mail.polimi.it>
2025-02-17 22:55:02 +0100

    Adds support for custom fonts

--- Traces.swiftpm/Traces.swift
+++ Traces.swiftpm/Traces.swift
@@ -7,6 +7,9 @@ import SwiftUI
 @main
 struct Traces: App {
     var body: some Scene {
+        // Loads and applies custom font to the app
+        let playwriteURL = Bundle.main.url(forResource: "Playwrite", withExtension: "ttf")! as CFURL
+        let _ = CTFontManagerRegisterFontsForURL(playwriteURL, CTFontManagerScope.process, nil)
         WindowGroup {
             Main()
         }
