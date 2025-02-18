5fb18a2440e9b90cfaee8bc204e37cd222019cf4
AstroWLAN <dario2.crippa@mail.polimi.it>
2025-02-18 17:59:28 +0100

    Updates digits view

--- Traces.swiftpm/Views/Tabs/Digits.swift
+++ Traces.swiftpm/Views/Tabs/Digits.swift
@@ -5,11 +5,46 @@
 import SwiftUI
 
 struct Digits: View {
+    // Grid structure
+    private let gridColumns = Array(repeating: GridItem(.flexible(minimum: 5), spacing: 0), count: 5)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                // Grid layout to visualize all the digits in an organized manner
+                LazyVGrid(columns: gridColumns, alignment: .center, spacing: 40) {
+                    ForEach(0..<10, id: \.self) { index in
+                        Number(currentDigit: index)
+                    }
+                }
+            }
+            .padding(.horizontal, 80)
+            .scrollIndicators(.hidden)
+        }
     }
 }
 
-#Preview {
-    Digits()
+struct Number : View {
+    private let digits : [Int : String] = [0:"Zero", 1:"One", 2:"Two", 3:"Three", 4:"Four", 5:"Five", 6:"Six", 7:"Seven", 8:"Eight", 9:"Nine"]
+    @State var currentDigit : Int
+    @State private var selectedDigit : Int = 0
+    var body: some View {
+        ZStack {
+            Image(digits[currentDigit] ?? "Missing")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 120)
+            VStack(spacing: 0)  {
+                Spacer()
+                Text(digits[currentDigit] ?? "Missing")
+                    .font(.custom("Playwrite", size: 17))
+                    .foregroundStyle(Color(.systemGray))
+                    .outline(color: .white, width: 2)
+            }
+            .padding(.bottom, 10)
+        }
+        .background(Color(.systemGray6))
+        .clipShape(RoundedRectangle(cornerRadius: 16))
+        .shadow(color: Color(.systemGray5), radius: 2)
+    }
 }
